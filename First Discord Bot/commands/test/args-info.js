1cbea8eefded913144804c2ab93f4db719b150e5
Playbird12 <83607406+Playbird12@users.noreply.github.com>
2021-05-03 18:49:03 -0500

    Update args-info.js

--- First Discord Bot/commands/test/args-info.js
+++ First Discord Bot/commands/test/args-info.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'args-info',
 	description: 'Information about the arguments provided.',
 	args: true,
-	delete: false,
+	delete: true,
 	execute(message, args) {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar');
